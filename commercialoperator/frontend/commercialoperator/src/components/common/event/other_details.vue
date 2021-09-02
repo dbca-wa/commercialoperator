a403bd365533c45086e597101ee6674349e2041c
preranaandure <prerana.andure@dbca.wa.gov.au>
2021-09-02 11:29:07 +0800

    Payment fees and charges section for Event application.

--- commercialoperator/frontend/commercialoperator/src/components/common/event/other_details.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/event/other_details.vue
@@ -118,6 +118,32 @@
             </div>                
         </div>
     </div>
+
+    <div class="col-sm-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Payment of Fees and Charges<small></small>
+                <a class="panelClicker" :href="'#'+mBody" data-toggle="collapse"  data-parent="#userInfo" expanded="true" :aria-controls="mBody">
+                <span class="glyphicon glyphicon-chevron-up pull-right "></span>
+                </a>
+                </h3>
+            </div>
+            <div class="panel-body collapse in" :id="mBody">
+                <div class="" >                        
+                    <div class="form-horizontal col-sm-12">
+                        <div class="form-group">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <label>Depending on the filming operation, fees and charges may apply. Please see commercial filming <a :href="fees_and_charges" target="_blank">website</a> for information on fees and charges.</label>
+                                </div>   
+                            </div>
+                            <div class="row">&nbsp;</div>
+                        </div>
+                   </div>
+                </div>
+            </div>                
+        </div>
+    </div>
     
     <div class="col-sm-12">
         <div class="panel panel-default">
@@ -272,6 +298,17 @@ export default {
                 if(vm.global_settings){
                     for(var i=0; i<vm.global_settings.length; i++){
                         if(vm.global_settings[i].key=='credit_facility_link'){
+                            return vm.global_settings[i].value;
+                        }
+                    }
+                }
+                return '';
+            },
+            fees_and_charges: function(){
+                let vm=this;
+                if(vm.global_settings){
+                    for(var i=0; i<vm.global_settings.length; i++){
+                        if(vm.global_settings[i].key=='fees_and_charges'){
                             return vm.global_settings[i].value;
                         }
                     }
