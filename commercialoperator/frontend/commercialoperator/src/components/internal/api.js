1433b164c0b0ec04a8a624c8a0a99edf4d6fb85c
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2021-09-24 12:33:34 +0800

    Countries api fix

--- commercialoperator/frontend/commercialoperator/src/components/internal/api.js
+++ commercialoperator/frontend/commercialoperator/src/components/internal/api.js
@@ -1,6 +1,7 @@
 module.exports = {
     organisations: '/api/organisations.json',
-    countries: 'https://restcountries.eu/rest/v1/?fullText=true',
+    //countries: 'https://restcountries.eu/rest/v1/?fullText=true',
+    countries: '/api/countries',
     users: '/api/users.json',
     profile: '/api/profile',
 }
