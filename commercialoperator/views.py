3316e44e9da1cc4adee90a9c0938aa4f994ef8f8
Ashley <ash.woodiss-field@dbca.wa.gov.au>
2024-02-12 02:00:41 +0000

    Update: management command page only accessible by internal users

--- commercialoperator/views.py
+++ commercialoperator/views.py
@@ -153,8 +153,11 @@ class HelpView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class ManagementCommandsView(LoginRequiredMixin, TemplateView):
+class ManagementCommandsView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
     template_name = 'commercialoperator/mgt-commands.html'
+
+    def test_func(self):
+        return is_internal(self.request)
 
     def post(self, request):
         data = {}
