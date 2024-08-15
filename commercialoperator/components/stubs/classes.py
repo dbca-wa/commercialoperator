c429b456718b24d31cd6e0819f183e70eb19f100
Karsten Prehn <karsten.prehn@gmail.com>
2024-08-15 06:40:37 +0000

    Provided substiute classes for ledger address and organisation

--- commercialoperator/components/stubs/classes.py
+++ commercialoperator/components/stubs/classes.py
@@ -3,3 +3,23 @@ from django.db import models
 class Ersatz(models.Model):
     class Meta:
         abstract = True
+
+class ErsatzAddress(Ersatz):
+
+    line1 = models.Field()
+    line2 = models.Field()
+    locality = models.Field()
+    state = models.Field()
+    postcode = models.Field()
+
+    class Meta:
+        abstract = True
+
+class ErsatzOrganisation(Ersatz):
+
+    identification = models.Field()
+    name = models.Field()
+    abn = models.Field()
+
+    class Meta:
+        abstract = True
