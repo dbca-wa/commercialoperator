a1a300e67f59330742cd2ce63831abe6d51ebf00
Karsten Prehn <karsten.prehn@gmail.com>
2024-08-23 03:26:09 +0000

    Fixed get_fields and get_ordering not taking a getter object parameter anymore

--- commercialoperator/components/proposals/api.py
+++ commercialoperator/components/proposals/api.py
@@ -384,9 +384,8 @@ class ProposalFilterBackend(DatatablesFilterBackend):
             if date_to:
                 queryset = queryset.filter(proposal__lodgement_date__lte=date_to)
 
-        getter = request.query_params.get
-        fields = self.get_fields(getter)
-        ordering = self.get_ordering(getter, fields)
+        fields = self.get_fields(request)
+        ordering = self.get_ordering(request, view, fields)
         queryset = queryset.order_by(*ordering)
         if len(ordering):
             queryset = queryset.order_by(*ordering)
