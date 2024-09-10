39b82c9a1ad1d87f070ec3d05be407fb2052e613
Karsten Prehn <karsten.prehn@gmail.com>
2024-09-10 06:21:08 +0000

    Started adding proper datatable imports to main.js

--- commercialoperator/frontend/commercialoperator/src/main.js
+++ commercialoperator/frontend/commercialoperator/src/main.js
@@ -4,14 +4,49 @@ import Vue from 'vue';
 import resource from 'vue-resource';
 import App from './App';
 import router from './router';
-// eslint-disable-next-line no-unused-vars
-import bs from 'bootstrap';
+import 'bootstrap';
 import helpers from '@/utils/helpers';
+import { extendMoment } from 'moment-range';
+
+// require('datatables.net');
+// require('datatables.net-bs')();
+// require('datatables.net-responsive-bs')(window, $);
+// require('datatables.net-buttons/js/dataTables.buttons.js')(window, $);
+// require('datatables.net-buttons/js/buttons.html5.js')(window, $, jsZip);
+
+import 'datatables.net';
+import 'datatables.net-bs';
+import 'datatables.net-buttons';
+// import 'datatables.net-buttons-bs';
+import 'datatables.net-responsive';
+// import 'datatables.net-responsive-bs';
+import 'datatables.net-buttons/js/dataTables.buttons.js';
+import 'datatables.net-buttons/js/buttons.html5.js';
+
+import jsZip from 'jszip';
+window.JSZip = jsZip;
+
+import 'select2';
+import 'jquery-validation';
+
+import 'sweetalert2/dist/sweetalert2.css';
+import '@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import 'select2/dist/css/select2.min.css';
+import 'select2-bootstrap-5-theme/dist/select2-bootstrap-5-theme.min.css';
+import '@/../node_modules/datatables.net-bs/css/dataTables.bootstrap.min.css';
+// import '@/../node_modules/datatables.net-responsive-bs5/css/responsive.bootstrap5.min.css';
+
 // eslint-disable-next-line no-unused-vars
-import hooks from './packages';
+// import hooks from './packages';
 import api_endpoints from './api';
-require('../node_modules/bootstrap/dist/css/bootstrap.css');
+
+// require('../node_modules/bootstrap/dist/css/bootstrap.css');
 // require('../node_modules/font-awesome/css/font-awesome.min.css');
+
+require('@/../node_modules/@fortawesome/fontawesome-free/css/all.min.css');
+require('@/../node_modules/datatables.net-bs/css/dataTables.bootstrap.min.css');
+
+extendMoment(moment);
 
 Vue.config.devtools = true;
 Vue.config.productionTip = false;
