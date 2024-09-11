d109b2869ce44e7ec803eeb3d4fcc980a170284c
Karsten Prehn <karsten.prehn@gmail.com>
2024-09-11 04:01:34 +0000

    Added a comment to main.js to indicate which dt import needs to be resolved

--- commercialoperator/frontend/commercialoperator/src/main.js
+++ commercialoperator/frontend/commercialoperator/src/main.js
@@ -12,7 +12,7 @@ import 'datatables.net';
 import 'datatables.net-bs';
 import 'datatables.net-buttons';
 import 'datatables.net-responsive';
-// import 'datatables.net-buttons-bs';
+// import 'datatables.net-buttons-bs'; // This one causes compile error
 import 'datatables.net-responsive-bs';
 import 'datatables.net-buttons/js/dataTables.buttons.js';
 import 'datatables.net-buttons/js/buttons.html5.js';
