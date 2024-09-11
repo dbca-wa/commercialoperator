7f18e0b4bd64d5555c199b79d1ffb5caa8179e29
Karsten Prehn <karsten.prehn@gmail.com>
2024-09-11 02:05:06 +0000

    Formatted router/index.js

--- commercialoperator/frontend/commercialoperator/src/router/index.js
+++ commercialoperator/frontend/commercialoperator/src/router/index.js
@@ -1,25 +1,24 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
-import Profile from '@/components/user/profile.vue'
-import external_routes from '@/components/external/routes'
-import internal_routes from '@/components/internal/routes'
-Vue.use(Router)
+import Vue from 'vue';
+import Router from 'vue-router';
+import Profile from '@/components/user/profile.vue';
+import external_routes from '@/components/external/routes';
+import internal_routes from '@/components/internal/routes';
+Vue.use(Router);
 
 export default new Router({
     mode: 'history',
     routes: [
         {
-          path: '/firsttime',
-          name: 'first-time',
-          component: Profile
+            path: '/firsttime',
+            name: 'first-time',
+            component: Profile,
         },
         {
-          path: '/account',
-          name: 'account',
-          component: Profile
+            path: '/account',
+            name: 'account',
+            component: Profile,
         },
         external_routes,
-        internal_routes
-    ]
-})
+        internal_routes,
+    ],
+});
