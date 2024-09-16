8d52f916183afcf7fb5320f4030edff6ef88b300
Karsten Prehn <karsten.prehn@gmail.com>
2024-09-16 03:40:35 +0000

    Formatted date-field.vue

--- commercialoperator/frontend/commercialoperator/src/components/forms/date-field.vue
+++ commercialoperator/frontend/commercialoperator/src/components/forms/date-field.vue
@@ -2,78 +2,139 @@
     <div>
         <div class="form-group">
             <label :id="id">{{ label }}</label>
-            
+
             <template v-if="help_text">
                 <HelpText :help_text="help_text" />
             </template>
             <template v-if="help_text_assessor && assessorMode">
-                <HelpText  :help_text="help_text_assessor" assessorMode={assessorMode} isForAssessor={true} />
-            </template> 
+                <HelpText
+                    :help_text="help_text_assessor"
+                    assessor-mode="{assessorMode}"
+                    is-for-assessor="{true}"
+                />
+            </template>
 
             <template v-if="help_text_url">
                 <HelpTextUrl :help_text_url="help_text_url" />
             </template>
             <template v-if="help_text_assessor_url && assessorMode">
-                <HelpTextUrl :help_text_url="help_text_assessor_url" assessorMode={assessorMode} isForAssessor={true} />
-            </template> 
-
+                <HelpTextUrl
+                    :help_text_url="help_text_assessor_url"
+                    assessor-mode="{assessorMode}"
+                    is-for-assessor="{true}"
+                />
+            </template>
 
             <template v-if="assessorMode && !assessor_readonly">
                 <template v-if="!showingComment">
-                    <a v-if="comment_value != null && comment_value != undefined && comment_value != ''" href="" @click.prevent="toggleComment"><i style="color:red" class="fa fa-comment-o">&nbsp;</i></a>
-                    <a v-else href="" @click.prevent="toggleComment"><i class="fa fa-comment-o">&nbsp;</i></a>
+                    <a
+                        v-if="
+                            comment_value != null &&
+                            comment_value != undefined &&
+                            comment_value != ''
+                        "
+                        href=""
+                        @click.prevent="toggleComment"
+                        ><i style="color: red" class="fa fa-comment-o"
+                            >&nbsp;</i
+                        ></a
+                    >
+                    <a v-else href="" @click.prevent="toggleComment"
+                        ><i class="fa fa-comment-o">&nbsp;</i></a
+                    >
                 </template>
-                <a href="" v-else  @click.prevent="toggleComment"><i class="fa fa-ban">&nbsp;</i></a>
+                <a v-else href="" @click.prevent="toggleComment"
+                    ><i class="fa fa-ban">&nbsp;</i></a
+                >
             </template>
-            <div class='input-group date'>
-                <input type="text" :readonly="readonly" :name="name" class="form-control" placeholder="DD/MM/YYYY" :value="value" :required="isRequired"/>
+            <div class="input-group date">
+                <input
+                    type="text"
+                    :readonly="readonly"
+                    :name="name"
+                    class="form-control"
+                    placeholder="DD/MM/YYYY"
+                    :value="value"
+                    :required="isRequired"
+                />
                 <span class="input-group-addon">
                     <span class="glyphicon glyphicon-calendar"></span>
                 </span>
             </div>
         </div>
-        <Comment :question="label" :readonly="assessor_readonly" :name="name+'-comment-field'" v-show="showingComment && assessorMode" :value="comment_value"/> 
+        <Comment
+            v-show="showingComment && assessorMode"
+            :question="label"
+            :readonly="assessor_readonly"
+            :name="name + '-comment-field'"
+            :value="comment_value"
+        />
     </div>
 </template>
 
 <script>
-import moment from 'moment'
-// import datetimepicker from 'datetimepicker'
-import Comment from './comment.vue'
-import HelpText from './help_text.vue'
-import HelpTextUrl from './help_text_url.vue'
+import Comment from './comment.vue';
+import HelpText from './help_text.vue';
+import HelpTextUrl from './help_text_url.vue';
 export default {
-    props: ['name', 'label', 'id', 'readonly', 'help_text', 'help_text_assessor', 'assessorMode', 'value', 'conditions', "handleChange","comment_value","assessor_readonly", "isRequired", 'help_text_url', 'help_text_assessor_url'],
-    data(){
+    components: { Comment, HelpText, HelpTextUrl },
+    props: [
+        // eslint-disable-next-line vue/require-prop-types
+        'name',
+        // eslint-disable-next-line vue/require-prop-types
+        'label',
+        // eslint-disable-next-line vue/require-prop-types
+        'id',
+        // eslint-disable-next-line vue/require-prop-types
+        'readonly',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'help_text',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'help_text_assessor',
+        // eslint-disable-next-line vue/require-prop-types
+        'assessorMode',
+        // eslint-disable-next-line vue/require-prop-types
+        'value',
+        // eslint-disable-next-line vue/require-prop-types
+        'conditions',
+        // eslint-disable-next-line vue/require-prop-types
+        'handleChange',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'comment_value',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'assessor_readonly',
+        // eslint-disable-next-line vue/require-prop-types
+        'isRequired',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'help_text_url',
+        // eslint-disable-next-line vue/prop-name-casing, vue/require-prop-types
+        'help_text_assessor_url',
+    ],
+    data() {
         return {
-            showingComment: false
-        }
+            showingComment: false,
+        };
     },
-    components: {Comment, HelpText, HelpTextUrl},
     computed: {
-        isChecked: function() {
-        //TODO return value from database
-        return false;
+        isChecked: function () {
+            //TODO return value from database
+            return false;
         },
-        options: function() {
-        return JSON.stringify(this.conditions);
-        }
+        options: function () {
+            return JSON.stringify(this.conditions);
+        },
     },
-    methods:{
-        toggleComment(){
-            this.showingComment = ! this.showingComment;
-        }
+    mounted: function () {},
+    methods: {
+        toggleComment() {
+            this.showingComment = !this.showingComment;
+        },
     },
-    mounted: function() {
-        // $('.date').datetimepicker({
-        // format: 'DD/MM/YYYY'
-        // });
-    }
-}
+};
 </script>
 
 <style lang="css">
-    input {
-        box-shadow:none;
-    }
+input {
+    box-shadow: none;
+}
 </style>
