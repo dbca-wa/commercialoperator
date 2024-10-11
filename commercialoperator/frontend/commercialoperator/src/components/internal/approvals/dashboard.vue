52656a96e6e6f32a7053a4c25ceee121d4f1cc79
Karsten Prehn <karsten.prehn@gmail.com>
2024-10-11 03:03:21 +0000

    Implemted formsection for internal approvals dashboard

--- commercialoperator/frontend/commercialoperator/src/components/internal/approvals/dashboard.vue
+++ commercialoperator/frontend/commercialoperator/src/components/internal/approvals/dashboard.vue
@@ -1,19 +1,22 @@
 <template>
     <div id="internalDash" class="container">
-        <ApprovalDashTable level="internal" :url="approvals_url" />
+        <FormSection :form-collapse="false" label="Licences" index="approvals">
+            <ApprovalDashTable level="internal" :url="approvals_url" />
+        </FormSection>
     </div>
 </template>
 <script>
+import FormSection from '@/components/forms/section_toggle.vue';
 import ApprovalDashTable from '@common-utils/approvals_dashboard.vue';
 import { api_endpoints } from '@/utils/hooks';
 export default {
     name: 'InternalApprovalDashboard',
     components: {
+        FormSection,
         ApprovalDashTable,
     },
     data() {
         return {
-            //approvals_url: api_endpoints.approvals,
             approvals_url: api_endpoints.approvals_paginated_external,
         };
     },
