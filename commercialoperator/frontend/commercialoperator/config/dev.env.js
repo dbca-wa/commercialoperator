3e551631f58b516bfd3cf2005e62883512d2387a
Jawaid Mushtaq <jawaid_mushtaq@yahoo.com>
2024-11-01 09:49:32 +0800

    Updated app.js port, removed bpoint_ledger_payment_audit_report from crom

--- commercialoperator/frontend/commercialoperator/config/dev.env.js
+++ commercialoperator/frontend/commercialoperator/config/dev.env.js
@@ -4,6 +4,6 @@ var prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   //WEBPACK_HOST: '"172.31.1.50:8080"',
-  WEBPACK_HOST: '"localhost:9072"',
-  PORT: '"9072"'
+  WEBPACK_HOST: '"localhost:9107"',
+  PORT: '"9107"'
 })
