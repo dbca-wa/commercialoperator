b038f3345aea16449c2e212781c212a2846a5462
Karsten Prehn <karsten.prehn@gmail.com>
2025-03-19 06:55:45 +0000

    Added a constants.js

---
+++ commercialoperator/frontend/commercialoperator/src/utils/constants.js
@@ -0,0 +1,13 @@
+export default {
+    SUPPORT_EMAIL: '?.?@dbca.wa.gov.au',
+
+    ERRORS: {
+        NETWORK_ERROR: `NETWORK ERROR: Please check your internet connection and try again. If the problem persists contact us at: ?.?@dbca.wa.gov.au`,
+        API_ERROR:
+            'API ERROR: An error has occurred accessing the Commercial Operator System API. Please try again \
+            in an hour and if the problem persists contact us at: ?.?@dbca.wa.gov.au',
+        API_ERROR_INTERNAL:
+            'API ERROR: An error has occurred accessing the Commercial Operator System API. Please try again \
+            in an hour and if the problem persists contact the IT Service Desk.',
+    },
+};
