a65f6f2c3eeeadf489dbd92b3471c2d3664ca633
Karsten Prehn <karsten.prehn@gmail.com>
2025-04-15 04:20:02 +0000

    Added a fetchLinkedOrganisation util fn

--- commercialoperator/frontend/commercialoperator/src/utils/utils.js
+++ commercialoperator/frontend/commercialoperator/src/utils/utils.js
@@ -96,4 +96,31 @@ export default {
                 });
         });
     },
+    /**
+     * Returns the linked organisation a the given ledger organisation id
+     * @param {Number} id A ledger organisation id
+     * @returns a Promise that resolves to the linked organisation
+     */
+    fetchLinkedOrganisation: function (id) {
+        return new Promise((resolve, reject) => {
+            const url =
+                api_endpoints.organisation +
+                `/linked_organisation?org_id=${id}`;
+            fetch(url)
+                .then(async (response) => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        const error =
+                            (data && data.message) || response.statusText;
+                        console.error(error);
+                        reject(error);
+                    }
+                    resolve(data);
+                })
+                .catch((error) => {
+                    console.error('There was an error!', error);
+                    reject(error);
+                });
+        });
+    },
 };
