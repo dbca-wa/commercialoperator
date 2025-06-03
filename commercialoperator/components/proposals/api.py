9adec9cebc93127ab703a31f4122ed5f628a6795
Karsten Prehn <karsten.prehn@gmail.com>
2025-06-03 04:23:35 +0000

    Removed a comment

--- commercialoperator/components/proposals/api.py
+++ commercialoperator/components/proposals/api.py
@@ -195,7 +195,6 @@ class GetEmptyList(views.APIView):
 """
 
 
-# class ProposalFilterBackend(DatatablesFilterBackend):
 class ProposalFilterBackend(LedgerDatatablesFilterBackend):
     """
     Custom filters
