ebd8a5a14afd2207f23763e14b6aafe5741a4778
Karsten Prehn <karsten.prehn@gmail.com>
2025-06-05 02:55:11 +0000

    Added raise_exception kwarg to rgetattr which if True makes the method throw a value error if the attr does not exist

--- commercialoperator/components/segregation/mixins.py
+++ commercialoperator/components/segregation/mixins.py
@@ -41,11 +41,34 @@ class MembersPropertiesMixin:
 class RecursiveGetAttributeMixin:
 
     @basic_exception_handler
-    def rgetattr(self, obj, attr, *args):
+    def rgetattr(self, obj, attr, *args, **kwargs):
+        """Recursively get an attribute from an object or a dictionary.
+        This method allows to access nested attributes using dot notation.
+
+        Args:
+            obj: The object or dictionary to retrieve the attribute from.
+            attr: The attribute name, which can be a nested path (e.g. "a.b.c").
+            *args: Additional arguments to pass to `getattr
+            kwargs: Additional keyword arguments, including:
+                - raise_exception: If True, raises an exception if the attribute is not found.
+                                  Defaults to False.
+        Returns:
+            The value of the attribute if found, otherwise None.
+        Raises:
+            ValueError: If `raise_exception` is True and the attribute is not found.
+        """
+
+        raise_exception = kwargs.get("raise_exception", False)
+
         def _getattr(obj, attr):
             if isinstance(obj, dict):
                 return obj.get(attr, None)
             else:
-                return getattr(obj, attr, *args) if hasattr(obj, attr) else None
+                has_attr = hasattr(obj, attr)
+                if raise_exception and not has_attr:
+                    raise ValueError(
+                        f"Attribute '{attr}' not found in {obj.__class__.__name__}"
+                    )
+                return getattr(obj, attr, *args) if has_attr else None
 
         return functools.reduce(_getattr, [obj] + attr.split("."))
