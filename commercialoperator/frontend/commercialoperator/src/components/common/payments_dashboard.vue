6524fb99d6e001b115d916404e09b33e0bf33172
Karsten Prehn <karsten.prehn@gmail.com>
2025-06-12 01:43:17 +0000

    Changed initial ordering of payments dashboard table to lodgement number descending

--- commercialoperator/frontend/commercialoperator/src/components/common/payments_dashboard.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/payments_dashboard.vue
@@ -272,6 +272,7 @@ export default {
                 },
                 responsive: true,
                 serverSide: true,
+                order: [[0, 'desc']],
                 lengthMenu: [
                     [10, 25, 50, 100, -1],
                     [10, 25, 50, 100, 'All'],
