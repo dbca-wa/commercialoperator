b950fe5673154343740f3fd28b655ae84943a719
Karsten Prehn <karsten.prehn@gmail.com>
2025-08-25 06:09:50 +0000

    Formatted two submit vue components

--- commercialoperator/frontend/commercialoperator/src/components/external/proposal_submit.vue
+++ commercialoperator/frontend/commercialoperator/src/components/external/proposal_submit.vue
@@ -1,37 +1,80 @@
 <template lang="html">
-    <div class="container" >
+    <div class="container">
         <div class="row">
             <div class="col-sm-12">
                 <div class="row">
-                    <div v-if="isProposal" class="col-sm-offset-3 col-sm-6 borderDecoration">
-                      <div v-if="proposal.application_type==application_type_filming">
-                        <strong>Your commercial filming application has been successfully submitted.</strong>
-                        <br/>
-                        
-                      </div>
-                      <div v-else>
-                        <strong>Your application for a commercial operations licence has been successfully submitted.</strong>
-                        <br/>
-                      </div>
+                    <div
+                        v-if="isProposal"
+                        class="col-sm-offset-3 col-sm-6 borderDecoration"
+                    >
+                        <div
+                            v-if="
+                                proposal.application_type ==
+                                application_type_filming
+                            "
+                        >
+                            <strong
+                                >Your commercial filming application has been
+                                successfully submitted.</strong
+                            >
+                            <br />
+                        </div>
+                        <div v-else>
+                            <strong
+                                >Your application for a commercial operations
+                                licence has been successfully submitted.</strong
+                            >
+                            <br />
+                        </div>
                         <!-- <strong>Your application for a commercial operations licence has been successfully submitted.</strong>
                         <br/> -->
                         <table>
                             <tr>
                                 <td><strong>Application:</strong></td>
-                                <td><strong>{{proposal.lodgement_number}}</strong></td>
+                                <td>
+                                    <strong>{{
+                                        proposal.lodgement_number
+                                    }}</strong>
+                                </td>
                             </tr>
                             <tr>
                                 <td><strong>Date/Time:</strong></td>
-                                <td><strong> {{proposal.lodgement_date|formatDate}}</strong></td>
+                                <td>
+                                    <strong>
+                                        {{
+                                            proposal.lodgement_date | formatDate
+                                        }}</strong
+                                    >
+                                </td>
                             </tr>
                         </table>
-                        <br/>
-                        <label>You will receive a notification email if there is any incomplete information or documents missing from the application.</label>
-                        <router-link :to="{name:'external-proposals-dash'}" style="margin-top:15px;" class="btn btn-primary">Back to home</router-link>
+                        <br />
+                        <label
+                            >You will receive a notification email if there is
+                            any incomplete information or documents missing from
+                            the application.</label
+                        >
+                        <router-link
+                            :to="{ name: 'external-proposals-dash' }"
+                            style="margin-top: 15px"
+                            class="btn btn-primary"
+                            >Back to home</router-link
+                        >
                     </div>
-                    <div v-else class="col-sm-offset-3 col-sm-6 borderDecoration">
-                        <strong>Sorry it looks like there isn't any application currently in your session.</strong>
-                        <br /><router-link :to="{name:'external-proposals-dash'}" style="margin-top:15px;" class="btn btn-primary">Back to home</router-link>
+                    <div
+                        v-else
+                        class="col-sm-offset-3 col-sm-6 borderDecoration"
+                    >
+                        <strong
+                            >Sorry it looks like there isn't any application
+                            currently in your session.</strong
+                        >
+                        <br /><router-link
+                            :to="{ name: 'external-proposals-dash' }"
+                            style="margin-top: 15px"
+                            class="btn btn-primary"
+                            >Back to home</router-link
+                        >
                     </div>
                 </div>
             </div>
@@ -39,53 +82,44 @@
     </div>
 </template>
 <script>
-import Vue from 'vue'
-import {
-  api_endpoints,
-  helpers
-}
-from '@/utils/hooks'
-import utils from './utils'
+import { api_endpoints } from '@/utils/hooks';
 export default {
-  data: function() {
-    let vm = this;
-    return {
-        "proposal": {},
-    }
-  },
-  components: {
-  },
-  computed: {
-    isProposal: function(){
-      return this.proposal && this.proposal.id ? true : false;
+    components: {},
+    filters: {
+        formatDate: function (data) {
+            return data ? moment(data).format('DD/MM/YYYY HH:mm:ss') : '';
+        },
     },
-    application_type_tclass: function(){
-      return api_endpoints.t_class;
+    beforeRouteEnter: function (to, from, next) {
+        next((vm) => {
+            vm.proposal = to.params.proposal;
+        });
     },
-    application_type_filming: function(){
-      return api_endpoints.filming;
+    data: function () {
+        return {
+            proposal: {},
+        };
     },
-    application_type_event: function(){
-      return api_endpoints.event;
-    }
-  },
-  methods: {
-  },
-  filters:{
-        formatDate: function(data){
-            return data ? moment(data).format('DD/MM/YYYY HH:mm:ss'): '';
-        }
-  },
-  mounted: function() {
-    let vm = this;
-    vm.form = document.forms.new_proposal;
-  },
-  beforeRouteEnter: function(to, from, next) {
-    next(vm => {
-        vm.proposal = to.params.proposal;
-    })
-  }
-}
+    computed: {
+        isProposal: function () {
+            return this.proposal && this.proposal.id ? true : false;
+        },
+        application_type_tclass: function () {
+            return api_endpoints.t_class;
+        },
+        application_type_filming: function () {
+            return api_endpoints.filming;
+        },
+        application_type_event: function () {
+            return api_endpoints.event;
+        },
+    },
+    mounted: function () {
+        let vm = this;
+        vm.form = document.forms.new_proposal;
+    },
+    methods: {},
+};
 </script>
 
 <style lang="css" scoped>
