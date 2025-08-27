ce0b060ad5f264f5e3e32ac7a9e5064460e100a9
Karsten Prehn <karsten.prehn@gmail.com>
2025-08-27 04:07:10 +0000

    Fixed reissue license throwing a database error after segregation

--- commercialoperator/components/proposals/models.py
+++ commercialoperator/components/proposals/models.py
@@ -2558,7 +2558,9 @@ class Proposal(DirtyFieldsMixin, RevisionedMixin):
             elif self.approval and self.approval.can_reissue:
                 if (
                     self.__approver_group()
-                    in request.user.proposalapprovergroup_set.all()
+                    in retrieve_user_groups(
+                    "proposalapprovergroup", request.user.id
+                )
                 ):
                     self.processing_status = status
                     # self.save()
