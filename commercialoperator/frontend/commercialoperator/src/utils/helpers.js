ee31eb63db394fff26706e0b5cdc072788a99a3f
Karsten Prehn <karsten.prehn@gmail.com>
2025-09-04 03:12:50 +0000

    Added a fetchUrl helper function. Need to replace all occurrences of $http.get/post with fetch throughout the project

--- commercialoperator/frontend/commercialoperator/src/utils/helpers.js
+++ commercialoperator/frontend/commercialoperator/src/utils/helpers.js
@@ -1,4 +1,35 @@
+const ERRORS = {
+    NETWORK_ERROR: `NETWORK ERROR: Please check your internet connection and try again.`,
+};
+
 module.exports = {
+    fetchUrl: async function (url, options) {
+        return new Promise((resolve, reject) => {
+            let f = options === undefined ? fetch(url) : fetch(url, options);
+            f.then(
+                async (response) => {
+                    const data = await response.json();
+                    if (!response.ok) {
+                        let error =
+                            (data.constructor.name === 'Array' && data) ||
+                            (data && data.message) ||
+                            response.statusText;
+                        console.error(error);
+                        reject(error);
+                    }
+                    resolve(data);
+                },
+                (error) => {
+                    console.error(
+                        `There was an error fetching from ${url}`,
+                        error
+                    );
+                    error = new Error(ERRORS.NETWORK_ERROR);
+                    reject(error);
+                }
+            );
+        });
+    },
     apiError: function (resp) {
         var error_str = '';
         if (resp.status === 400) {
