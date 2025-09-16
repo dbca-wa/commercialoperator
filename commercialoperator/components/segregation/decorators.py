1f380d886d80a833cd04194271389f679814d261
Karsten Prehn <karsten.prehn@gmail.com>
2025-09-16 03:02:53 +0000

    Added status object as code instead of just a number

--- commercialoperator/components/segregation/decorators.py
+++ commercialoperator/components/segregation/decorators.py
@@ -5,7 +5,7 @@ import traceback
 
 from django.conf import settings
 from django.core.exceptions import ValidationError
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.exceptions import APIException
 
 logger = logging.getLogger(__name__)
@@ -21,7 +21,7 @@ def basic_exception_handler(func):
         except serializers.ValidationError as e:
             raise e
         except NotImplementedError as e:
-            raise APIException(code=501, detail=str(e))
+            raise APIException(code=status.HTTP_501_NOT_IMPLEMENTED, detail=str(e))
         except Exception as e:
             if settings.DEBUG:
                 detail = {
