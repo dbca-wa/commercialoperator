486edd1749772c1112509bd3983161fa4e91371d
NishantPhour <phournishant0@gmail.com>
2026-07-01 10:06:03 +0800

    expire_approvals filter changed to also check the approvals that have been replaced

--- commercialoperator/management/commands/expire_approvals.py
+++ commercialoperator/management/commands/expire_approvals.py
@@ -22,8 +22,9 @@ class Command(BaseCommand):
         updates = []
         today = timezone.localtime(timezone.now()).date()
         logger.info("Running command {}".format(__name__))
-        # for a in Approval.objects.filter(status="current"):
-        for a in Approval.objects.filter(status="current", replaced_by__isnull=True):
+        # we need to check all approvals
+        for a in Approval.objects.filter(status="current"):
+        # for a in Approval.objects.filter(status="current", replaced_by__isnull=True):
             if a.expiry_date < today:
                 try:
                     a.expire_approval(user)
